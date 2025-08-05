3446bfc8219e11a25acaeb5029eb4d7300131ad9
JeongMin-981 <jeongmin981@gmail.com>
2025-08-04 19:08:32 -0700

    [fix] fix repo

--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import utils
-from . import config
-from . import model
-from . import network
--- lib/model/__init__.py
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------
-# Reference from HRNet-Human-Pose-Estimation
-# refact code from old one.
-# Written by Jeongmin Kim(jm.kim@dankook.ac.kr)
-# ----------------------------------------------------
-from .model import get_fcn
